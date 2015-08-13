ff5527fc40103ac412f9f1b842b52d9b4593eb3b
Jonas Hecht <jonas.hecht@codecentric.de>
2015-08-13 15:34:57 +0200

    Fix for double-pretty-print (which leads to empty lines in the In-Messages)

--- src/main/java/de/codecentric/soap/configuration/WebServiceMessageLoggerConfiguration.java
+++ src/main/java/de/codecentric/soap/configuration/WebServiceMessageLoggerConfiguration.java
@@ -1,7 +1,11 @@
 package de.codecentric.soap.configuration;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.cxf.bus.spring.SpringBus;
-import org.apache.cxf.feature.LoggingFeature;
+import org.apache.cxf.interceptor.AbstractLoggingInterceptor;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,13 +18,26 @@ public class WebServiceMessageLoggerConfiguration {
 	@Autowired
 	private SpringBus springBus;
 		
-	@Bean
-	public LoggingFeature loggingFeature() {
+	@PostConstruct
+	public void activateLoggingFeature() {
 		// Log SoapMessages to Logfile
-    	LoggingFeature logFeature = new LoggingFeature();
-    	logFeature.setPrettyLogging(true);
-    	logFeature.initialize(springBus);
-    	springBus.getFeatures().add(logFeature);
-    	return logFeature;
+    	springBus.getInInterceptors().add(logInInterceptor());
+    	springBus.getInFaultInterceptors().add(logInInterceptor());
+    	springBus.getOutInterceptors().add(logOutInterceptor());
+    	springBus.getOutFaultInterceptors().add(logOutInterceptor());
+	}
+
+	@Bean
+	public AbstractLoggingInterceptor logInInterceptor() {
+		LoggingInInterceptor logInInterceptor = new LoggingInInterceptor();
+		// The In-Messages are pretty without setting it
+		return logInInterceptor; 
+	}
+	
+	@Bean
+	public AbstractLoggingInterceptor logOutInterceptor() {
+		LoggingOutInterceptor logOutInterceptor = new LoggingOutInterceptor();
+		logOutInterceptor.setPrettyLogging(true);
+		return logOutInterceptor; 
 	}
 }
