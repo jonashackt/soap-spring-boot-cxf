4c53228f9035c6937cd30957dc77af7d4f6393c2
Jonas Hecht <jonas.hecht@codecentric.de>
2016-04-04 11:22:52 +0200

    Fix for new deps, PDFBox changed packages, spring-boot has newer logback-core than logback-logstash-encoder

--- src/test/java/de/codecentric/soap/endpoint/WeatherServiceEndpointTest.java
+++ src/test/java/de/codecentric/soap/endpoint/WeatherServiceEndpointTest.java
@@ -9,7 +9,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
